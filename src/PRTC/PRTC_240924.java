2e49c744c70388dfa5b1a227c0ee91d1785de3b8
thx-to <tlsekghk@gmail.com>
2024-09-24 19:31:23 +0900

    local updated

---
+++ src/PRTC/PRTC_240924.java
@@ -0,0 +1,57 @@
+package PRTC;
+// 백준 JAVA 입력시 public class 이름은 Main으로 바꿔서 제출, 범위는 Public class 전체
+// 백준 입출력과 사칙연산
+
+import java.util.Scanner;
+
+public class PRTC_240924 {
+    public static void main(String[] args) {
+
+        /*
+        // 2557 Hello World
+        // Hello World!를 출력하시오.
+        System.out.println("Hello World!");
+        */
+
+        /*
+        // 1000 A + B, 1001 A - B, 10998 A x B
+        // 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+        Scanner sc = new Scanner(System.in);
+        int A, B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.print(A + B);
+        */
+
+        /*
+        // 1008 A / B
+        // 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+        Scanner sc = new Scanner(System.in);
+        double A, B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.print(A / B);
+        */
+
+        /*
+        // 10869 사칙연산
+        // 두 자연수 A와 B가 주어진다.
+        // 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+        Scanner sc = new Scanner(System.in);
+        int A, B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+        */
+
+
+
+
+    }
+
+
+}
